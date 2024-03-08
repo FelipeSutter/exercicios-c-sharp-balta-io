3ee9c2f96513a9c12b8dbc81025102865b60b6cb
FelipeSutter <lipeps61@gmail.com>
2024-03-08 11:48:59 -0300

    feat: projeto Numbers finalizado

---
+++ Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+
+        decimal valor = 10534.25m;
+
+        Console.WriteLine(Math.Round(valor)); // para configurar a moeda de cada país
+    }
+}
