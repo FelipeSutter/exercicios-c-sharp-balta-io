535577d767b3790e1b09f81f6839cecd5e9e6f08
FelipeSutter <lipeps61@gmail.com>
2024-03-08 10:42:21 -0300

    feat: trabalhando com timezones

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -1,17 +1,33 @@
-﻿using System.Text;
+﻿using System.Globalization;
 
 class Program {
 
     public static void Main(string[] args) {
 
-        var texto = new StringBuilder();
-        texto.Append("Este texto é um teste");
-        texto.Append("Este texto é um teste");
-        texto.Append("Este texto é um teste");
-        texto.Append("Este texto é um teste");
-        texto.Append("Este texto é um teste");
+        // Localização e Globalização de Data e Hora
 
-        Console.WriteLine(texto.ToString());
+        Console.Clear();
+
+        var dateTime = DateTime.UtcNow; // hora global
+
+        Console.WriteLine(dateTime);
+        Console.WriteLine(dateTime.ToLocalTime());
+
+        var timezoneAustralia = TimeZoneInfo.FindSystemTimeZoneById("Pacific/Auckland");
+        Console.Write(timezoneAustralia.ToString());
+
+        var horaAustralia = TimeZoneInfo.ConvertTimeFromUtc(dateTime, timezoneAustralia);
+        Console.WriteLine(horaAustralia);
+
+        // lista todos os tempos cadastrados.
+        var timezones = TimeZoneInfo.GetSystemTimeZones();
+        foreach (var timezone in timezones)
+        {
+            Console.WriteLine(timezone.Id);
+            Console.WriteLine(timezone);
+            Console.WriteLine(TimeZoneInfo.ConvertTimeFromUtc(dateTime, timezone));
+            Console.WriteLine("--------------");
+        }
 
     }
 
