083e13fcb32d13cb37ef3fbef08999be6ae557a0
FelipeSutter <lipeps61@gmail.com>
2024-03-08 14:47:17 -0300

    feat: Arrays e Exceptions finalizado

---
+++ Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+
+
+        var outroArray = new int[5] { 1, 2, 3, 4, 5 };
+
+        foreach (int i in outroArray)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        var array = new int[3];
+        
+        try
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // System.IndexOutOfRangeException
+                Console.WriteLine(array[i]);
+            }
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.InnerException);
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Não encontrei o índice");
+        }
+        
+        catch(Exception ex)
+        {
+            Console.Write("Ops, deu errado!");   
+        }
+
+
+    }
+
+    public class MinhaException : Exception
+    {
+        public DateTime WhatTime { get; set; }
+    }
+
+}
